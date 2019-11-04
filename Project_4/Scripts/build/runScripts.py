0cf085a001b3f209f91c5ce8951a080b86af51b7
Oline <oline.a.ranum@gmail.com>
2019-11-04 16:07:49 +0100

    Everything is brilliant again, completed assignment, halfway tex

--- Project_4/Scripts/build/runScripts.py
+++ Project_4/Scripts/build/runScripts.py
@@ -6,6 +6,6 @@ for i  in range(Number_of_files):
     os.system("mpirun -n 4 ./Project_4")
 
     directory = "../../Py_Scripts/Results/4E/WrittenFiles/"
-    old_file = os.path.join(directory, "L_100_0.txt")
-    new_file = os.path.join(directory, "L_100_" + str(i) + ".txt")
+    old_file = os.path.join(directory, "L_80_0.txt")
+    new_file = os.path.join(directory, "L_80_" + str(i) + ".txt")
     os.rename(old_file, new_file)
